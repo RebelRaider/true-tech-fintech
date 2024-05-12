bc30ea700098ebfb7556bc0798d672c9d55fa324
Максим Шатохин <soldboy407@gmail.com>
2024-05-12 17:47:09 +0300

    Body

--- src/components/MobileVersion/MobileVersion.jsx
+++ src/components/MobileVersion/MobileVersion.jsx
@@ -3,7 +3,11 @@ import styles from './MobileVersion.module.css';
 const MobileVersion = () => {
     return (
         <div className={styles.MainContainer}>
-
+            <div className={styles.SectorContainer}>
+                <div className={styles.HeaderContainer}></div>
+                <div className={styles.BodyContainer}></div>
+                <div className={styles.FooterPanel}></div>
+            </div>
         </div>
     )
 }
